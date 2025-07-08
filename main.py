c004cde55609c7f7f99d52c0398dc667303b8844
AlexNT-maker <alexnter98@gmail.com>
2025-07-08 17:30:08 +0300

    The why_track html file has finished, also his styling with animations

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def home():
     """ Render the home page with a form to input a URL. """
     return render_template('welcome.html')
 
+@app.route('/why_track')
+def why_track():
+    """ Render the 'Why Track' page. """
+    return render_template('why_track.html')
+
 
 if __name__ == '__main__':
     # Run the Flask application
